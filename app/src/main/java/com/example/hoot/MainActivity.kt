9d9510096c929917bb30136dc786c3de5d87d5ed
Flor Patrone <patroneflorenciacarla@gmail.com>
2021-02-14 20:16:49 -0300

    Agregué detección de campo de nombre vacío.

--- app/src/main/java/com/example/hoot/MainActivity.kt
+++ app/src/main/java/com/example/hoot/MainActivity.kt
@@ -2,12 +2,20 @@ package com.example.hoot
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_start.setOnClickListener {
+            if (et_name.text.toString().isEmpty()) {
+                Toast.makeText(this, "Por favor ingresa un nombre", Toast.LENGTH_SHORT).show()
+            }
+        }
 
     }
 }
