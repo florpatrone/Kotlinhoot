32f65ce22855765feb413111c54b20674cd590de
Flor Patrone <patroneflorenciacarla@gmail.com>
2021-02-15 21:15:09 -0300

    Creé pantalla de preguntas con botones [V3]

--- app/src/main/java/com/example/hoot/PreguntasActivity.kt
+++ app/src/main/java/com/example/hoot/PreguntasActivity.kt
@@ -3,6 +3,7 @@ package com.example.hoot
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import kotlinx.android.synthetic.main.activity_preguntas.*
 
 class PreguntasActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,5 +12,17 @@ class PreguntasActivity : AppCompatActivity() {
 
         val listaPreguntas = Constantes.obtenerPreguntas()
         Log.i("Tamanio preguntas", "${listaPreguntas.size}")
+
+        val posActual = 1
+        val pregunta: PreguntaChoice? = listaPreguntas[posActual - 1]
+
+        progress_bar.progress = posActual
+        tv_progress.text = "$posActual" + "/" + progress_bar.max
+        tv_question.text = pregunta!!.pregunta
+        tv_option_one.text = pregunta.opcionUno
+        tv_option_two.text = pregunta.opcionDos
+        tv_option_three.text = pregunta.opcionTres
+        tv_option_four.text = pregunta.opcionCuatro
+        tv_option_five.text = pregunta.opcionCinco
     }
 }
