a08d1ef75514d174492e2c4259934f7becbb13c9
Flor Patrone <patroneflorenciacarla@gmail.com>
2021-02-24 01:17:25 -0300

    Se conecta exitosamente

--- app/src/main/java/com/example/hoot/ApiService.kt
+++ app/src/main/java/com/example/hoot/ApiService.kt
@@ -6,13 +6,12 @@ import retrofit2.http.Query
 
 interface ApiService {
     companion object {
-        val BASE_URL: String = "https://run.mocky.io/v3/f6027d09-9c43-4ea3-bedf-0041a8003fad/"
+        //val BASE_URL: String = "https://run.mocky.io/v3/f6027d09-9c43-4ea3-bedf-0041a8003fad/"
+        val BASE_URL: String = "http://3.141.216.104:8080/api/v1/"
+        //val BASE_URL: String = "http://127.0.0.1:8080/api/v1/"
     }
 
-    @GET("search")
-    suspend fun getResult(
-        //@Query("q")
-        //query:String?
-    ):Response<List<Pregunta>>
+    @GET("preguntas")
+    suspend fun getResult():Response<List<Pregunta>>
 
 }
